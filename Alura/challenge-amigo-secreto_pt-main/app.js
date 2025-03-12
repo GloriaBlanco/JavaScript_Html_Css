c7cde5b159f9902b1f0249b6b52fd646c77a5601
gloriablanco <gloriablanco@uol.com.br>
2025-03-11 21:26:11 -0300

    Challenge-amigo-secreto-Alura

---
+++ Alura/challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,76 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let AmigosIncluidos = [];
+
+let nomesSorteados = [];
+
+function adicionarAmigo() { // este nome da função está no arquiv input  no html
+    // pega o nome do amigo digitado no input no html
+    let nome = document.getElementById("amigo").value;
+    // verifica se o nome é válido    
+    if (!nome) {
+        alert('Por favor, insira um nome válido');
+        console.log(nome);
+        return;
+    }
+    // verifica se o nome já esta na lista lista incluirAmigos
+    if (AmigosIncluidos.includes(nome)){
+        alert('Este nome já foi adicionado.Por favor digite outro nome.');
+        console.log(nome);
+        document.getElementById("amigo").value = "";
+        document.getElementById("amigo").focus();
+        return;
+    }
+    // adiciona o nome na lista lista_amigos
+    AmigosIncluidos.push(nome);
+    console.log(nome);
+    console.log(AmigosIncluidos);
+    // limpa o campo de input // coloca o foco no campo de input
+    document.getElementById("amigo").value = "";
+    document.getElementById("amigo").focus();
+    atualizarListaAmigos(); // atualiza a lista de amigos
+}
+
+// função para mostrar os nomes digitados na lista na tela para o usuário 
+function atualizarListaAmigos() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+    for (let i = 0; i < AmigosIncluidos.length; i++){         
+       let elemento = document.createElement("li");
+       elemento.textContent = AmigosIncluidos[i];
+       listaAmigos.appendChild(elemento);
+    }
+}
+
+// esta funcao sorteia um amigo para o usuario, e mostra na tela
+function sortearAmigo() { // este nome da funçaõ é o mesmo que esta no botão no html
+    if (AmigosIncluidos.length < 2) {
+        alert('Por favor, adicione pelo menos dois amigos para sortear.');
+        return;
+    }
+    let amigoSorteado = AmigosIncluidos[Math.floor(Math.random() * AmigosIncluidos.length)];
+    console.log(amigoSorteado);
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `O amigo sorteado foi ${amigoSorteado}`;
+    // Adiciona o amigo sorteado à lista de nomes sorteados
+    nomesSorteados.push(amigoSorteado);
+    // excluir o  nome sorteado da lista de AmigosIncluidos
+    AmigosIncluidos.splice(AmigosIncluidos.indexOf(amigoSorteado), 1);
+    // atualiza a lista de amigos    console.log(AmigosIncluidos);
+    console.log(nomesSorteados);
+    console.log(AmigosIncluidos);
+    atualizarListaAmigosSorteados();
+}
+
+// função para mostrar os nomes sorteados na lista na tela para o usuário
+function atualizarListaAmigosSorteados() {
+    let Sorteados= document.getElementById("Sorteados");
+    Sorteados.innerHTML = "";
+    for (let i = 0; i < nomesSorteados.length; i++){   
+       let elementoSorteado = document.createElement("li");
+       elementoSorteado.textContent = nomesSorteados[i];
+       Sorteados.appendChild(elementoSorteado);
+    }
+    console.log(nomesSorteados);
+}
+
